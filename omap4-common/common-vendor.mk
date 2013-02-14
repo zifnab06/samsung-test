de43f5d34a20386c0bf3178d5485d74dd5112034
codeworkx <codeworkx@cyanogenmod.org>
2013-02-14 17:56:36 +0000

    i9100g: update blobs from LSD, add omap4 common blobs

--- omap4-common/common-vendor.mk
+++ omap4-common/common-vendor.mk
@@ -12,3 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Pick up overlay for features that depend on non-open-source files
+DEVICE_PACKAGE_OVERLAYS += vendor/samsung/omap4-common/overlay
+
+$(call inherit-product, vendor/samsung/omap4-common/common-vendor-blobs.mk)
